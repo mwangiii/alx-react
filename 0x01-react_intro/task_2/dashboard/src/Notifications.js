c6c40d0fd6ced57468662cafffa13e0b42c5ad93
mwangiii <mwangi8kevin@gmail.com>
2023-10-26 10:21:34 +0300

    data priority

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -11,7 +11,7 @@ export default function Notifications () {
       <p>Here is the list of notifications</p>
       <ul>
         <li data-priority='default'>New course available</li>
-        <li data-priority='priority'>New resume available</li>
+        <li data-priority='urgent'>New resume available</li>
         <li data-priority='urgent' dangerouslySetInnerHTML={{ __html: getLatestNotification() }} />
       </ul>
       <button
