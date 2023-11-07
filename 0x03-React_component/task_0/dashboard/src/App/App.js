b6e0fa5ecdd1e1f3b9667bfbc3eb84cc3bb7c69f
mwangiii <mwangi8kevin@gmail.com>
2023-11-07 19:23:51 +0300

    remove constructor

--- 0x03-React_component/task_0/dashboard/src/App/App.js
+++ 0x03-React_component/task_0/dashboard/src/App/App.js
@@ -10,37 +10,34 @@ import { getLatestNotification } from '../utils/utils';
 const listCourses = [
   { id: 1, name: 'ES6', credit: 60 },
   { id: 2, name: 'Webpack', credit: 20 },
-  { id: 3, name: 'React', credit: 40 },
+  { id: 3, name: 'React', credit: 40 }
 ];
 const listNotifications = [
   { id: 1, type: 'default', value: 'New course available' },
   { id: 2, type: 'urgent', value: 'New resume available' },
-  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } },
+  { id: 3, type: 'urgent', html: { __html: getLatestNotification() } }
 ];
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-  render() {
+  render () {
     const { isLoggedIn } = this.props;
     return (
-      <Fragment>
+      <>
         <Notifications listNotifications={listNotifications} />
         <Header />
         {isLoggedIn ? <CourseList listCourses={listCourses} /> : <Login />}
         <Footer />
-      </Fragment>
+      </>
     );
   }
 }
 
 App.defaultProps = {
-  isLoggedIn: false,
+  isLoggedIn: false
 };
 
 App.propTypes = {
-  isLoggedIn: PropTypes.bool,
+  isLoggedIn: PropTypes.bool
 };
 
 export default App;
