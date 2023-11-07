863502e3eac238aa311a6987a5dc9ee2ce53627f
mwangiii <mwangi8kevin@gmail.com>
2023-11-07 23:01:45 +0300

    test files

--- 0x03-React_component/task_1/dashboard/src/App/App.test.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.test.js
@@ -1,47 +1,49 @@
+import { shallow } from 'enzyme';
 import React from 'react';
-import { shallow, mount } from 'enzyme';
 import App from './App';
-import Header from './Header/Header';
-import Login from './Login/Login';
-import Footer from './Footer/Footer';
-import Notifications from './Notifications/Notifications';
-import CourseList from './CourseList/CourseList'
-  
-describe('App', () => {
-  it('renders properly', () => {
-    shallow(<App />);
-  });
 
-  it('checking renders', () => {
+describe('<App />', () => {
+  it('renders without crashing', () => {
     const wrapper = shallow(<App />);
-    expect(wrapper.find(Header)).toHaveLength(1);
-    expect(wrapper.find(Login)).toHaveLength(1);
-    expect(wrapper.find(Footer)).toHaveLength(1);
-    expect(wrapper.find(Notifications)).toHaveLength(1);
   });
-})
 
-describe('display when logged in', () => {
-  it ('logged in true', () => {
-    const wrapper = shallow(<App isLoggedIn="true" />);
-    expect(wrapper.find(CourseList).exists()).toBeTruthy();
-    expect(wrapper.find(Login).exists()).not.toBeTruthy();
+  it('contain Notifications component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Notifications')).toHaveLength(1);
   });
-})
 
-describe('testing App as a class component', () => {
-  it('control+h test', () => {
-    const alertMock = jest.fn();
-  
-    const spy = jest.spyOn(App.prototype, 'componentDidMount');
-    const wrapper = mount(<App isLoggedIn="true" />);
-    expect(spy).toHaveBeenCalled();
-    const event = new KeyboardEvent('keydown', {
-      key: 'control',
-    });
+  it('contain Header component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Header')).toHaveLength(1);
+  });
 
-    spy.mockReset();
-    spy.mockRestore();
-    jest.clearAllMocks()
-  })
-})
+  it('contain Login component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Login')).toHaveLength(1);
+  });
+
+  it('contain Footer component', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('Footer')).toHaveLength(1);
+  });
+
+  it('CourseList', () => {
+    const wrapper = shallow(<App />);
+    expect(wrapper.find('CourseList')).toHaveLength(0);
+  });
+
+  it('isLoggedIn true', () => {
+    const wrapper = shallow(<App isLoggedIn />);
+    expect(wrapper.find('Login')).toHaveLength(0);
+    expect(wrapper.find('CourseList')).toHaveLength(1);
+  });
+
+  it('logOut', () => {
+    const logOut = jest.fn(() => undefined);
+    const wrapper = shallow(<App logOut={logOut} />);
+    const alert = jest.spyOn(global, 'alert');
+    expect(alert);
+    expect(logOut);
+    jest.restoreAllMocks();
+  });
+});
